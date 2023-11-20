5c802d738a0076d81cdec189944e6a7af558af2e
WORKSTATION-DM <daniel.mosselman@outlook.com>
2023-11-20 22:07:46 +0100

    changed numbers

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 
-	var randomX int = (rand.Int() * 100)
-	var randomY int = (rand.Int() * 100)
+	var randomX int = 10
+	var randomY int = 5
 	var sum int = sum(randomX, randomY)
 	fmt.Println("Sum of", randomX, "and", randomY, "is", sum)
 }
